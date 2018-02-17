23a7083c6408af818c1addbd3fc143c09506525f
unknown <mkap25@yahoo.com>
2018-02-17 02:16:33 -0500

    pushing up , just tested API

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+   consumer_key: 'vfmZdvRtkpLE66ge1I99ejM4H',
+   consumer_secret: 'v3qEkMIHk1UvqbLi0LOeSNeJNIto4quJW60bz03oJWOzIC3aAg',
+   access_token: '1014883080-t1USWqebPgRmAN4LZpsBgQXSShsNPE0u3SQxe6O',
+   access_token_secret: '9X58Qq4HEVf0IAkhFSN87Y2Vua6bzLhghqV0EVMP4uMCC'
+}
